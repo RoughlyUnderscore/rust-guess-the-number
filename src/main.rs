aa1eeb4de069ba599f414ebbe2ae9c72700cbaf5
Roughly_ <75266403+RoughlyUnderscore@users.noreply.github.com>
2023-08-11 19:15:25 +0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::{*, io::Write};
 use rand::prelude::*;
+use std::cmp::Ordering;
+use std::{io::Write, *};
 
 fn main() {
   loop {
@@ -21,24 +22,38 @@ fn main() {
  * Returns the amount of guesses.
  */
 fn game_loop() -> i32 {
-  let mut lower_limit: i32;
-  let mut upper_limit: i32;
+  let mut lower_limit: i128 = 0;
+  let mut upper_limit: i128 = 0;
   loop {
-    (lower_limit, upper_limit) = input_loop();
+    let (lower_limit_string, upper_limit_string) = input_loop();
+    match (
+      lower_limit_string.trim().parse::<i128>(),
+      upper_limit_string.trim().parse::<i128>(),
+    ) {
+      (Ok(num1), Ok(num2)) => {
+        lower_limit = num1;
+        upper_limit = num2;
+      }
+      (Err(_), Err(_)) => (),
+      (Ok(_), Err(_)) => (),
+      (Err(_), Ok(_)) => (),
+    };
 
-    if lower_limit < upper_limit {
-      break;
+    if lower_limit != 0 && upper_limit != 0 {
+      if lower_limit < upper_limit {
+        break;
+      } else {
+        println!("The lower limit must be lower than the upper limit!");
+      }
     }
-
-    println!("The lower limit must be lower than the upper limit!");
   }
 
   let mut rng = thread_rng();
-  let number = rng.gen_range(lower_limit .. upper_limit);
+  let number = rng.gen_range(lower_limit..upper_limit);
 
   println!("Attempt to guess the number.");
-  
-  let mut current_guess: i32;
+
+  let mut current_guess: i128;
   let mut guesses = 0;
 
   loop {
@@ -49,12 +64,10 @@ fn game_loop() -> i32 {
     println!("{}", input.trim());
     current_guess = input.trim().parse().unwrap();
 
-    if current_guess > number {
-      println!("Your guess is higher than the number.");
-    } else if current_guess < number {
-      println!("Your guess is lower than the number.");
-    } else {
-      break;
+    match current_guess.cmp(&number) {
+      Ordering::Less => println!("Your guess is too low."),
+      Ordering::Greater => println!("Your guess is too high"),
+      Ordering::Equal => break,
     }
 
     guesses += 1;
@@ -64,7 +77,7 @@ fn game_loop() -> i32 {
   guesses
 }
 
-fn input_loop() -> (i32, i32) {
+fn input_loop() -> (String, String) {
   let mut lower_limit_string = String::new();
   let mut upper_limit_string = String::new();
 
@@ -74,10 +87,7 @@ fn input_loop() -> (i32, i32) {
   immediate_print("Upper limit: ");
   io::stdin().read_line(&mut upper_limit_string).unwrap();
 
-  let lower_limit: i32 = lower_limit_string.trim().parse().unwrap();
-  let upper_limit: i32 = upper_limit_string.trim().parse().unwrap();
-
-  (lower_limit, upper_limit)
+  (lower_limit_string, upper_limit_string)
 }
 
 fn immediate_print(arg: &str) {
